2bfcf94621351a4bf1669d5087c2e1f436db2968
tonyco972 <antonio.colapietro97@gmail.com>
2025-04-28 10:33:55 +0200

    Update update_gist.py

--- update_gist.py
+++ update_gist.py
@@ -10,7 +10,7 @@ GIST_ID = "47554691172b680172f003458025d7c3"
 GIST_FILENAME = "data.json"
 
 # Carica il token GitHub dall'ambiente
-GITHUB_TOKEN = os.getenv("GIST_TOKEN")
+GITHUB_TOKEN = os.getenv("REPO")
 
 def scrape_news():
     """

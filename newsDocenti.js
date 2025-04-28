63d47339f14893593dd41dffe1838be2e62845be
tonyco972 <antonio.colapietro97@gmail.com>
2025-04-28 11:27:50 +0200

    Update newsDocenti.js

--- newsDocenti.js
+++ newsDocenti.js
@@ -6,7 +6,12 @@ const url = 'https://www.orizzontescuola.it/';
 
 async function fetchNews() {
   try {
-    const { data } = await axios.get(url);
+    const { data } = await axios.get(url, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+      }
+    });
+
     const $ = cheerio.load(data);
 
     const news = [];
